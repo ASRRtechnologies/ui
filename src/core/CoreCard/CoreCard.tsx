cdd167d069dc88fb56efa0dc1aaf97c87cdaa321
Amar97 <amar.ramdas@asrr.nl>
2023-03-08 17:43:44 +0100

    Finish up first version of core card

--- src/core/CoreCard/CoreCard.tsx
+++ src/core/CoreCard/CoreCard.tsx
@@ -1,13 +1,53 @@
-import {createStyles, Group, Paper} from '@mantine/core';
+import {createStyles, Group, Paper, rem, Text} from '@mantine/core';
+import {ArrowDownIcon, ArrowUpIcon, EllipsisVerticalIcon} from "@heroicons/react/24/solid";
 
-const useStyles = createStyles((theme) => ({}));
+const useStyles = createStyles((theme) => ({
+    group: {
+        width: "200px",
+        height: "300px"
+    },
+    value: {
+        fontSize: rem(24),
+        fontWeight: 700,
+        lineHeight: 1,
+    },
+    diff: {
+        lineHeight: 1,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    title: {
+        fontWeight: 700,
+        textTransform: 'uppercase',
+    },
+}));
 
 export function CoreCard() {
-  const { classes } = useStyles();
-  return <Paper shadow={"sm"} radius={"xl"}>
-    <Group>
-        <div>Hi</div>
-    </Group>
-    CoreCard
-  </Paper>;
+    const {classes} = useStyles();
+
+    let stat = {
+        diff: 30,
+    }
+
+    const DiffIcon = stat.diff > 0 ? ArrowUpIcon : ArrowDownIcon;
+    return <Paper withBorder shadow={"sm"} radius={"lg"} p="md" styles={classes.group}>
+        <Group position="apart">
+            <Text size="md" color="dimmed" className={classes.title}>
+                hi
+            </Text>
+            <EllipsisVerticalIcon width={24}/>
+        </Group>
+
+        <Group align="flex-end" spacing="xs" mt={25}>
+            <Text className={classes.value}>{1200}</Text>
+            <Text color={stat.diff > 0 ? 'teal' : 'red'} fz="sm" fw={500} className={classes.diff}>
+                <span>{stat.diff}%</span>
+                <DiffIcon height={12}/>
+            </Text>
+        </Group>
+
+        <Text fz="xs" c="dimmed" mt={7}>
+            Compared to previous month
+        </Text>
+    </Paper>;
 }
