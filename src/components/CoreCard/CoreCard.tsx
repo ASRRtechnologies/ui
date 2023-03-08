2140d1e5dbcecaf22301338e59a4ed7ef27977a3
Amar97 <amar.ramdas@asrr.nl>
2023-03-08 15:37:10 +0100

    Add plop script

--- src/components/CoreCard/CoreCard.tsx
+++ src/components/CoreCard/CoreCard.tsx
@@ -1,7 +1,10 @@
 
 import { createStyles } from '@mantine/core';
+
 const useStyles = createStyles((theme) => ({}));
+
 export function CoreCard() {
+
   const { classes } = useStyles();
   return <div>CoreCard</div>;
 }
